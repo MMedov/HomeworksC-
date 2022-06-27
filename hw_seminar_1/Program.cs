b3b75b7795eaa99947e380ccddbe77756997a657
Magomed <magamed06ing@yandex.ru>
2022-06-27 16:11:47 +0300

    1 задачка решена

---
+++ hw_seminar_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int current;
+int N;
+Console.Write("Введите число : ");
+N = Convert.ToInt32(Console.ReadLine());
+current = 2;
+while (current<=N)
+{
+Console.Write(current+" ");
+current = current+2;
+}
+
+if (N < 2)
+{
+    Console.Write("Введите число не меньше 2: ");
+
+}
