648d672e0e8073b3cd60865b38b3ec234dc7f9c5
Magomed <magamed06ing@yandex.ru>
2022-06-27 16:20:11 +0300

    some changes made

--- hw_seminar_1/Program.cs
+++ hw_seminar_1/Program.cs
@@ -1,16 +1,15 @@
-﻿int current;
-int N;
-Console.Write("Введите число : ");
-N = Convert.ToInt32(Console.ReadLine());
-current = 2;
-while (current<=N)
-{
-Console.Write(current+" ");
-current = current+2;
-}
+﻿Console.Write("Введите число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = 2;
 
 if (N < 2)
 {
     Console.Write("Введите число не меньше 2: ");
 
+}
+
+while (current<=N)
+{
+Console.Write(current+" ");
+current = current+2;
 }
