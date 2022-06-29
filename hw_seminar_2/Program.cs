34ba1c28e93cc7c96a522cb02cead8e4a4876010
Magomed <magamed06ing@yandex.ru>
2022-06-30 00:50:18 +0300

    first task completed

---
+++ hw_seminar_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите день недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void IsItWeekend(int num)
+{
+    if(num == 6 || num == 7)
+        Console.WriteLine("Да, это выходной!");
+        
+    else
+        if(num < 1 || num > 7)
+            Console.WriteLine("Я для тебя шутка какая-то?!");
+        else
+        Console.WriteLine("Нет, это не выходной!");
+}
+IsItWeekend(x);
