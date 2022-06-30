3e8783914c6e6aa83d560aae389fd930d26c160e
Magomed <magamed06ing@yandex.ru>
2022-06-30 22:02:43 +0300

    firs completed

---
+++ hw_seminar_3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int x)
+{
+    int current = 1;
+    while(current <= x )
+    {
+        int quad = current * current * current;
+        Console.Write(quad + " ");
+        current++;
+    }
+}
+
+if(N < 1) 
+    Console.Write("Введенное число меньше 1!");
+else
+    Cube(N);
+*/
+
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void IsPalindrom(int x)
+{
+    
+}
