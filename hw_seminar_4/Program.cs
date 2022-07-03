6343b64876e9f422e6a29929d6c4e932e57f5ef5
Magomed <magamed06ing@yandex.ru>
2022-07-04 02:52:48 +0300

    task form 3 lection

--- hw_seminar_4/Program.cs
+++ hw_seminar_4/Program.cs
@@ -1,2 +1,22 @@
 ﻿
 Console.WriteLine("Hello, World!");
+int[] five = {1, 6, 3, 0, 8};
+void FromMaxToMin(int[] array)
+{
+    
+    for(int i=0; i<array.Length; i++)
+    {
+        int Maximum = i;
+        for(int j=i+1; j<array.Length; j++)
+        {
+            if(array[j]>array[Maximum])
+            Maximum = j;
+        }
+        int temp = array[i];
+        array[i] = array[Maximum];
+        array[j] = temp;
+        
+    }
+}
+
+void FromMaxToMin(int[] array)
