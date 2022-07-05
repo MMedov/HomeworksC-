1b326770d4cc2f46f2019b8709551311db6bdacd
Magomed <magamed06ing@yandex.ru>
2022-07-05 23:11:13 +0300

    homework completed

--- hw_seminar_4/Program.cs
+++ hw_seminar_4/Program.cs
@@ -1,53 +1,4 @@
-﻿/*
-Console.WriteLine("Hello, World!");
-int[] five = {1, 6, 3, 0, 8};
-void FromMaxToMin(int[] array)
-{
-    
-    for(int i=0; i<array.Length; i++)
-    {
-        int Maximum = i;
-        for(int j=i+1; j<array.Length; j++)
-        {
-            if(array[j]>array[Maximum])
-            Maximum = j;
-        }
-        int temp = array[i];
-        array[i] = array[Maximum];
-        array[j] = temp;
-        
-    }
-}
-
-void FromMaxToMin(int[] array)
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+﻿
  //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 /*
 Console.Write("Введите число: ");
@@ -63,5 +14,28 @@ int SumOfDigits(int num)
     }
     return sum;
 }
+
 Console.Write("Сумма цифр введеного числа равна " + SumOfDigits(N));
 */
+
+//Напишите программу, которая задаёт массив из m элементов и выводит их на экран. 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+
+Console.Write("Массив из сколько элементов вам нужен? Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[m];
+Random rnd = new Random();
+
+void MassiveOfRandom(int x)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+
+MassiveOfRandom(m);
