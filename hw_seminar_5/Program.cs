5a93adedff3e7c0dc26a893fbf3c4f30ef0cdba4
Magomed <magamed06ing@yandex.ru>
2022-07-08 16:26:49 +0300

    second task completed

--- hw_seminar_5/Program.cs
+++ hw_seminar_5/Program.cs
@@ -13,6 +13,7 @@ void ShowArray(int[] array)
 
  */
 //Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
 int[] CreateRandomArray(int size, int min, int max) 
 { 
     int[] array = new int[size];  
@@ -20,6 +21,13 @@ int[] CreateRandomArray(int size, int min, int max)
     for(int i = 0; i < size; i++) 
     array[i] = new Random().Next(min, max); 
     return array; 
+} 
+void ShowArray(int[] array) 
+{ 
+    for(int i = 0; i < array.Length; i++) 
+        Console.Write(array[i] + " "); 
+ 
+    Console.WriteLine(); 
 } 
 int CountOfEvens(int[] array)
  {  int count = 0;
@@ -30,13 +38,6 @@ int CountOfEvens(int[] array)
 
     return count;
  }
-void ShowArray(int[] array) 
-{ 
-    for(int i = 0; i < array.Length; i++) 
-        Console.Write(array[i] + " "); 
- 
-    Console.WriteLine(); 
-} 
 
 int size = new Random().Next(10, 20);
 int min = 100;
@@ -44,3 +45,33 @@ int max = 999;
 int[] arr = CreateRandomArray(size, min, max); 
 ShowArray(arr);
 Console.Write(CountOfEvens(arr));
+*/
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] CreateRandomArray(int size, int min, int max) 
+{ 
+    int[] array = new int[size];  
+ 
+    for(int i = 0; i < size; i++) 
+    array[i] = new Random().Next(min, max); 
+    return array; 
+} 
+void ShowArray(int[] array) 
+{ 
+    for(int i = 0; i < array.Length; i++) 
+        Console.Write(array[i] + " "); 
+ 
+    Console.WriteLine(); 
+} 
+int SumOfOdds(int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i+=2) sum = sum + array[i];
+    return sum;
+}
+
+int size = new Random().Next(10, 20);
+int min = 0;
+int max = 9;
+int[] arr = CreateRandomArray(size, min, max); 
+ShowArray(arr);
+Console.Write(SumOfOdds(arr));
