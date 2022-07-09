78d3b3fe2e250e86fce51250e1d0e034e4c72bf6
Magomed <magamed06ing@yandex.ru>
2022-07-09 22:03:17 +0300

    finally finished

--- hw_seminar_5/Program.cs
+++ hw_seminar_5/Program.cs
@@ -39,7 +39,7 @@ int CountOfEvens(int[] array)
     return count;
  }
 
-int size = new Random().Next(10, 15);
+int size = new Random().Next(10, 15);//размер массива будет рандомным
 int min = 100;
 int max = 999;
 int[] arr = CreateRandomArray(size, min, max); 
@@ -70,7 +70,7 @@ int SumOfOddPositions(int[] array)
     return sum;
 }
 
-int size = new Random().Next(10, 15);
+int size = new Random().Next(10, 15); //размер массива будет рандомным
 int min = 0;
 int max = 9;
 int[] arr = CreateRandomArray(size, min, max); 
@@ -78,36 +78,44 @@ ShowArray(arr);
 Console.Write(SumOfOddPositions(arr));
 */
 //Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-int[] CreateRandomArray(int size, int min, int max) 
+
+//(тут я не совсем понял какие числа должны быть, поэтому написал метод создающий массив с  рандомными ...
+//... числами от min до max с двумя цифрами после запятой. Сам метод принимает диапазон в виде min, max и size из основной программы)
+double[] CreateRandomArray(int size, int min, int max) 
 { 
-    int[] array = new int[size];  
+    double[] array = new double[size];  
  
     for(int i = 0; i < size; i++) 
-    array[i] = new Random().Next(min, max); 
+    {
+        array[i] = new Random().NextDouble() * (max-min) + min;
+        array[i] = Math.Round(array[i], 2);
+    }
+    
     return array; 
 } 
-void ShowArray(int[] array) 
+void ShowArray(double[] array) 
 { 
     for(int i = 0; i < array.Length; i++) 
         Console.Write(array[i] + " "); 
  
     Console.WriteLine(); 
 } 
-int MaxMinDifference(int[] array)
+double MaxMinDifference(double[] array)
 {
-    int max = array[0];
-    int min = array[0];
+    double max = array[0];
+    double min = array[0];
     for(int i=1; i<array.Length; i++)
     {
         if(array[i] > max) max = array[i];
         if(array[i] < min) min = array[i];
     }
-    return max-min;
+    return Math.Round(max-min, 2);//без округления иногда выдавались значения с кучей цифр после запятой, несмотря на то, что в массиве таких чисел нет
 }
 
-int size = new Random().Next(10, 15);
+int size = new Random().Next(10, 15); //размер массива будет рандомным
 int min = 1;
-int max = 99;
-int[] arr = CreateRandomArray(size, min, max); 
+int max = 10;
+double[] arr = CreateRandomArray(size, min, max); 
 ShowArray(arr);
 Console.Write(MaxMinDifference(arr));
+//все работает как я задумал, а вот правильно ли я понял задачу это вопрос)
