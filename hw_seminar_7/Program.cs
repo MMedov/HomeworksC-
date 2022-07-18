8edbaf51c2df5802596ca2ea5c8cd686315f0050
Magomed <magamed06ing@yandex.ru>
2022-07-18 22:49:57 +0300

    secod task completed

--- hw_seminar_7/Program.cs
+++ hw_seminar_7/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
+/*
 double[,] Create2dArray(int rows, int columns, int min, int max)
 {
  double[,] array = new double[rows, columns];
@@ -26,7 +26,7 @@ void ShowArray(double[,] array)
         
 } 
 
-int min = 1;
+int min = -10;
 int max = 10;
 Console.WriteLine("Input number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -35,3 +35,55 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 double[,] arr = Create2dArray(m, n, min, max);
 ShowArray(arr);
+*/
+
+//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] Create2dArray(int rows, int columns, int min, int max)
+{
+ int[,] array = new int[rows, columns];
+ for(int i=0; i<array.GetLength(0); i++)
+ {
+    for(int j=0; j<array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(min, max);
+    }
+ }
+ return array;
+}
+void ShowArray(int[,] array) 
+{ 
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+        
+} 
+void AverageOfColumns(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+            sum = Math.Round(sum/array.GetLength(0),1);
+        Console.Write($"{sum} ");
+    }
+        
+}
+int min = 0;
+int max = 10;
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = Create2dArray(m, n, min, max);
+ShowArray(arr);
+Console.WriteLine();
+AverageOfColumns(arr);
