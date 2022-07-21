889e3abed11b796d0cfd1acf259041e1eb40be62
Magomed <magamed06ing@yandex.ru>
2022-07-21 22:06:21 +0300

    first task completed

---
+++ hw_seminar_8/Program.cs
@@ -0,0 +1,59 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+/*
+int[,] Create2dArray(int rows, int columns, int min, int max)
+{
+ int[,] array = new int[rows, columns];
+ for(int i=0; i<array.GetLength(0); i++)
+ {
+    for(int j=0; j<array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(min, max);
+    }
+ }
+ return array;
+}
+void ShowArray(int[,] array) 
+{ 
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+        
+} 
+
+int[,] SortRowsFromMinToMax(int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {   int x=array.GetLength(1)-1; 
+        while(x>0) 
+        { 
+            for(int j=0; j<array.GetLength(1)-1; j++)
+            {   
+                
+                if (array[i,j] > array[i,j + 1])
+                {
+                    int temp = array[i,j];
+                    array[i,j] = array[i,j+1];
+                    array[i,j+1] = temp;
+                }
+            }
+        x--;
+        }
+    }
+    return array;
+}
+
+int min = 0;
+int max = 9;
+int m = new Random().Next(4, 9); //Пусть массив будет рандомного размера, без запросов к пользователю
+int n = new Random().Next(4, 9);
+int[,] arr = Create2dArray(m, n, min, max);
+ShowArray(arr);
+SortRowsFromMinToMax(arr);
+Console.WriteLine();
+ShowArray(SortRowsFromMinToMax(arr));
+*/
